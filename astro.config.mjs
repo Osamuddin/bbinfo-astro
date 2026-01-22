805898bae7b8d93249ad24ad699d413878efadca
Osamu Maeda <osamumaeda@MacBook-Pro.local>
2026-01-22 14:36:26 +0530

    font upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
+  vite: {
+    build: {
+      // 10kb以下のファイルはインライン化（埋め込み）する設定
+      // これにより、8.0KiBのCSSファイルリクエストが消えます
+      assetsInlineLimit: 12288, 
+    },
+  },
 });
